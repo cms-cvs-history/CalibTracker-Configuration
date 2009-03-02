423131e81aa63e472595c942fc0086441fe46f54
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2009-03-02 14:56:38 +0000

    remove fake sources for LA and Gain. A GT with specific fake label has to be provided to allow the access of fake conditions for the simulation directly from the DB

--- python/Tracker_DependentRecords_forGlobalTag_nofakes_cff.py
+++ python/Tracker_DependentRecords_forGlobalTag_nofakes_cff.py
@@ -6,18 +6,11 @@ from Geometry.TrackerGeometryBuilder.trackerGeometry_cfi import *
 from CalibTracker.SiStripESProducers.SiStripGainESProducer_cfi import *
 
 #second SiStripGainESProducer is used in the digitizer and takes SiStripGainRcd from SiStripGainFakeSource
-from CalibTracker.SiStripESProducers.SiStripGainFakeSource_cfi import *
 import CalibTracker.SiStripESProducers.SiStripGainESProducer_cfi
 siStripGainESProducerforSimulation = CalibTracker.SiStripESProducers.SiStripGainESProducer_cfi.siStripGainESProducer.clone()
 siStripGainESProducerforSimulation.appendToDataLabel = 'fake'
-siStripGainESProducerforSimulation.APVGain = 'fakeAPVGain'
+siStripGainESProducerforSimulation.APVGain = 'fake'
 
-#Lorentz Angle
-#this SiStripLAFakeESSource is used by the digitizer
-import CalibTracker.SiStripESProducers.SiStripLAFakeESSource_cfi
-siStripLAFakeESSourceforSimulation = CalibTracker.SiStripESProducers.SiStripLAFakeESSource_cfi.siStripLAFakeESSource.clone()
-
-siStripLAFakeESSourceforSimulation.appendToDataLabel = 'fake'
 
 from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
 from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
